ccb767bbdab09572d5018cfeb023029692d3c66b
Guilherme Onizio <129892786+GuilhermeOnizio@users.noreply.github.com>
2024-02-05 20:12:39 -0300

    Add files via upload

---
+++ resolução.py
@@ -0,0 +1,9 @@
+#RESOLUÇÃO
+buttonfantasma = Button(perfil, width=20, height=2 , font="arial")
+buttonfantasma.place (x=80, y=400)
+buttonfantasma.config(background="#242424", highlightthickness=0, bd=0)
+
+perfil.mainloop()
+
+#Ao introduzir um botão fantasma, conseguimos resolver o problema de exibição da imagem que anteriormente não estava sendo exibida na tela.
+#Ao adicionar esse elemento invisível, a imagem passou a ser exibida corretamente, solucionando assim o bug de forma eficaz.
